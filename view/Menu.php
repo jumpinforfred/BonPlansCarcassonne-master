612afbecd618efc75f697bd764cee4ec9c3490b6
jumpinforfred <mcleanfreddie@gmail.com>
2016-03-12 21:25:48 +0100

    Test 1.0.1

--- view/Menu.php
+++ view/Menu.php
@@ -2,7 +2,6 @@
 </div>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -12,8 +11,6 @@
       </button>
       <a class="navbar-brand" href="#">Brand</a>
     </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
@@ -50,6 +47,5 @@
           </ul>
         </li>
       </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+    </div>
+  </div>
